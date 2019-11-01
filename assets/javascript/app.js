72df1d131cbe83d97bca3be4ba84ac85f1c38427
Dan Schissler <schisslerdg@gmail.com>
2019-10-31 21:20:55 -0400

    first draft

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+var questionsobject = {
+    question1 = {
+        query: "",
+        answers: [],
+    },
+    question2 = {
+        query: "",
+        answers: [],
+    },
+    question3 = {
+        query: "",
+        answers: [],
+    },
+    question4 = {
+        query: "",
+        answers: [],
+    },
+    question5 = {
+        query: "",
+        answers: [],
+    },
+    question6 = {
+        query: "",
+        answers: [],
+    },
+    question7 = {
+        query: "",
+        answers: [],
+    },
+    question8 = {
+        query: "",
+        answers: [],
+    }
+}
